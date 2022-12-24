6ece8c7393194b55e3d1a07e9ee10dc3708cba51
Nick <shuminskiy23@gmail.com>
2022-12-24 18:04:20 +0300

    change app settings

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import {Navigations} from "./src/navigation/navigations";
 import * as React from 'react';
 import {NativeBaseProvider} from "native-base";
+import {useEffect} from "react";
+import NetInfo from "@react-native-community/netinfo";
+import {createAlert} from "./src/common/components/alert";
+import {StatusBar} from 'expo-status-bar';
 
 const App = () => {
+    useEffect(() => {
+        NetInfo.addEventListener((state) => {
+            const offline = !(state.isConnected && state.isInternetReachable);
+            if (offline) {
+                createAlert({
+                    title: 'Сообщение',
+                    message: "Отсутствует подключение к интернету",
+                    buttons: [{text: 'Выйти', style: "cancel"}]
+                })
+            }
+        });
+    })
     return (
         <NativeBaseProvider>
-           <Navigations/>
+            <StatusBar hidden={false} style={'auto'} animated={true} translucent={true}/>
+            <Navigations/>
         </NativeBaseProvider>
     );
 }
